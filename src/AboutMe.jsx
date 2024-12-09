d9932e5e7e4901918151e9917492e4e02a4b391a
rishidil <131211616+rishidil@users.noreply.github.com>
2024-12-08 23:02:00 -0500

    added about me component

---
+++ src/AboutMe.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+
+const AboutMe = () =>{
+
+    return (
+        <div><p>this is the about me page</p></div>
+    )
+}
+
+export default AboutMe; 
